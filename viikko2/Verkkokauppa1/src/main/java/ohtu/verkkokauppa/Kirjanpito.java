1221b498360eee613732304531b3c5eb76ab30da
Otto Pirttimaa <otto.pirttimaa@gmail.com>
2019-11-11 21:08:52 +0200

    Add exercises 11 and 12

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
@@ -3,9 +3,14 @@ package ohtu.verkkokauppa;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Kirjanpito implements KirjanpitoInterface {
     private ArrayList<String> tapahtumat;
 
+    @Autowired
     public Kirjanpito() {
         this.tapahtumat = new ArrayList<String>();
     }
--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -1,8 +1,13 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Viitegeneraattori implements ViitegeneraattoriInterface {    
     private int seuraava;
     
+    @Autowired
     public Viitegeneraattori(){
         this.seuraava = 1;    
     }
